177eefa764842daca9340a6ef0dacb3399a963c2
caglaraydin <caglara@gmail.com>
2017-03-19 13:34:57 +0300

    Setting up Xamarin
    Created Lexer and Parser by using ANTLRWorks with CSharp3 language
    target

---
+++ CGATranslator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Antlr.Runtime;
+using Antlr.Runtime.Tree;
+
+namespace CGATranslator
+{
+	class MainClass
+	{
+		public static void Preorder (ITree Tree, int Depth)
+		{
+			if (Tree == null) {
+				return;
+			}
+
+			for (int i = 0; i < Depth; i++) {
+				Console.Write ("  ");
+			}
+
+			Console.WriteLine (Tree);
+
+			Preorder (Tree.GetChild (0), Depth + 1);
+			Preorder (Tree.GetChild (1), Depth + 1);
+		}
+
+		public static void Main (string [] args)
+		{
+			Stream inputStream = Console.OpenStandardInput ();
+			ANTLRInputStream input = new ANTLRInputStream (inputStream);
+			CGALexer lexer = new CGALexer (input);
+			CommonTokenStream tokens = new CommonTokenStream (lexer);
+			CGAParser parser = new CGAParser (tokens);                      // Create parser
+			var result = parser.ruleDefinition ();
+
+			CommonTree tree = (CommonTree)result.Tree;
+			//Console.WriteLine (tree.ToStringTree ());                     // Print out the tree
+			Preorder (tree, 0);                                             // Print out the tree with TAB GRAHP
+
+		}
+	}
+}
