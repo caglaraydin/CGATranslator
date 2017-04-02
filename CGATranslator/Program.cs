a7300dfad33b1d33be20c60ae003c36761873c2c
caglaraydin <caglara@gmail.com>
2017-04-02 15:00:07 +0300

    Reorganized and corrected Split definition in the grammar file.

--- CGATranslator/Program.cs
+++ CGATranslator/Program.cs
@@ -2,6 +2,11 @@
 using System.IO;
 using Antlr.Runtime;
 using Antlr.Runtime.Tree;
+using Antlr4.StringTemplate;
+using Antlr4.StringTemplate.Compiler;
+using Antlr4.StringTemplate.Debug;
+using Antlr4.StringTemplate.Extensions;
+using Antlr4.StringTemplate.Misc;
 
 namespace CGATranslator
 {
@@ -25,16 +30,26 @@ namespace CGATranslator
 
 		public static void Main (string [] args)
 		{
-			Stream inputStream = Console.OpenStandardInput ();
-			ANTLRInputStream input = new ANTLRInputStream (inputStream);
+			String inputString = " Lot --> A t(4,5,6) extrude(rand(2,32)) B ";
+			ANTLRStringStream input = new ANTLRStringStream (inputString);
+			
+			//Stream inputStream = Console.OpenStandardInput ();
+			//ANTLRInputStream input = new ANTLRInputStream (inputStream);
 			CGALexer lexer = new CGALexer (input);
 			CommonTokenStream tokens = new CommonTokenStream (lexer);
 			CGAParser parser = new CGAParser (tokens);                      // Create parser
 			var result = parser.ruleDefinition ();
 
 			CommonTree Tree = (CommonTree)result.Tree;
-			//Console.WriteLine (tree.ToStringTree ());                     // Print out the tree
-			Preorder (Tree, 0);                                             // Print out the tree with TAB GRAPH
+			Console.WriteLine (Tree.ToStringTree ());                     // Print out the tree
+
+			DotTreeGenerator gen = new DotTreeGenerator ();
+			var st = gen.ToDot (Tree);
+			Console.WriteLine (st);
+			//Console.WriteLine (TreeUtilities.toTree (inputStream).ToStringTree());
+
+			//Preorder (Tree, 0);                                             // Print out the tree with TAB GRAPH
+
 
 		}
 	}
